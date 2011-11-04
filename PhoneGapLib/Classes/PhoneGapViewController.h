17597e464f5d96ab5e7e6ab2b9331b0811320e2d
Gayane Minasyants <gayane.minasyants@rd-arts.com>
2011-11-04 02:04:45 -0700

    Added multiple view controllers support

--- PhoneGapLib/Classes/PhoneGapViewController.h
+++ PhoneGapLib/Classes/PhoneGapViewController.h
@@ -9,11 +9,58 @@
 #import <UIKit/UIKit.h>
 #import <Foundation/Foundation.h>
 
-@interface PhoneGapViewController : UIViewController {
+#import "JSONKit.h"
+
+@class InvokedUrlCommand;
+@class Sound;
+@class Contacts;
+@class Console;
+@class PGWhitelist;
+
+@interface PhoneGapViewController : UIViewController <UIWebViewDelegate>
+{
+    UIWebView* webView;
+    
+    NSString* _localRoot;
+    NSString* _startURL;
 }
+
+@property (nonatomic, copy) NSString* localRoot;
+@property (nonatomic, copy) NSString* startURL;
 
 @property (nonatomic, retain) 	NSArray* supportedOrientations;
 @property (nonatomic, retain)	UIWebView* webView;
 
+
+@property (nonatomic, retain) NSMutableDictionary *pluginObjects;
+@property (nonatomic, retain) NSDictionary *pluginsMap;
+@property (nonatomic, retain) NSDictionary *settings;
+@property (nonatomic, retain) PGWhitelist* whitelist;
+
+- (NSString*) pathForResource:(NSString*)resourcepath;
+
++ (NSDictionary*)getBundlePlist:(NSString *)plistName;
++ (NSString*) applicationDocumentsDirectory;
++ (NSString*) phoneGapVersion;
+
+- (int)executeQueuedCommands;
+- (void)flushCommandQueue;
+
+- (id) getCommandInstance:(NSString*)pluginName;
+- (void) javascriptAlert:(NSString*)text;
+- (BOOL) execute:(InvokedUrlCommand*)command;
+- (NSString*) appURLScheme;
+- (NSDictionary*) deviceProperties;
+
+- (NSString *)stringByEvaluatingJavaScriptFromString:(NSString *)javascript;
+
+- (void)urlWillBeLoaded:(NSURL* )url;
+
+@end
+
+
+@interface NSDictionary (LowercaseKeys)
+
+- (NSDictionary*) dictionaryWithLowercaseKeys;
 
 @end
